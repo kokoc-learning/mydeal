3df067cf524f453d11304d306afde7d6e0a43512
unknown <creator051@yandex.ru>
2020-01-28 07:45:08 +0300

    some fix

--- src/functions.php
+++ src/functions.php
@@ -53,5 +53,50 @@ function projectIdCheck($projectId, $projectList){
     return FALSE;
 }
 
-?>
+
+
+function db_get_prepare_stmt($con, $sql, $data = []) {
+    $stmt = mysqli_prepare($con, $sql);
+
+    if ($stmt === false) {
+        $errorMsg = 'Не удалось инициализировать подготовленное выражение: ' . mysqli_error($con);
+        die($errorMsg);
+    }
+
+    if ($data) {
+        $types = '';
+        $stmt_data = [];
+
+        foreach ($data as $value) {
+            $type = 's';
+
+            if (is_int($value)) {
+                $type = 'i';
+            }
+            else if (is_string($value)) {
+                $type = 's';
+            }
+            else if (is_double($value)) {
+                $type = 'd';
+            }
+
+            if ($type) {
+                $types .= $type;
+                $stmt_data[] = $value;
+            }
+        }
+
+        $values = array_merge([$stmt, $types], $stmt_data);
+
+        $func = 'mysqli_stmt_bind_param';
+        $func(...$values);
+
+        if (mysqli_errno($con) > 0) {
+            $errorMsg = 'Не удалось связать подготовленное выражение с параметрами: ' . mysqli_error($con);
+            die($errorMsg);
+        }
+    }
+
+    return $stmt;
+}
 
--- templates/add.php
+++ templates/add.php
@@ -1,14 +1,40 @@
 
 <?php
 
-if (isset($_POST['name']) && isset($_POST['project']) && isset($_POST['date'])){
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['name']) && isset($_POST['project'])){
+  $form = $_POST;
+
   $formNameValue = $_POST['name'];
   $formProjectValue = $_POST['project'];
   $formDateValue = $_POST['date'];
-  $formFileName = $_POST['file'];
-  // $formFileTmpPath = $_FILES['file'];
-  print_r(count($_FILES));
-  // echo "<br> $formFileTmpPath";
+ 
+  if (isset($_FILES['file']) && strlen($_FILES['file']['name']) > 0) {
+    
+    echo "<br> 0 ".strlen($_FILES['file']['name']);
+
+    // сгенерируем уникальное имя для сохраненного файла
+    // разобьем имя файла из формы (который загрузили) на массив со строками. 
+    // Разделителем будет точка (для выделения расширения файла)
+    $file_name = explode( '.', $_FILES['file']['name']);
+    // сгенерируем имя "прибавим" расширение оригинального файла
+    $file_name = uniqid().'.'.$file_name[1];
+    echo "<br> 1 ". $file_name;
+    
+    $file_path = ROOT_PATH . '/uploads/';
+    echo "<br> 2 ". $file_path;
+
+    $file_url = '/uploads/' . $file_name;
+    echo "<br> 3 ". $file_url;
+    
+    move_uploaded_file($_FILES['file']['tmp_name'], $file_path . $file_name);
+    $file_href = "<a download href='$file_url'>$file_name</a>";
+    echo "<br> 4 ". $file_href;
+  }
+}
+
+
+if(false) {
+  $sql = "INSERT INTO `task` (project_id, user_id, name, creation_date, deadline, status, file) VALUES (?, ?, ?, NOW(), ?, 0, ?)";
 }
   
 ?>
@@ -49,16 +75,18 @@ if (isset($_POST['name']) && isset($_POST['project']) && isset($_POST['date'])){
     <main class="content__main">
     <h2 class="content__main-heading">Добавление задачи</h2>
 
-<form class="form"  action="add.php" method="post" autocomplete="off">
+<form class="form"  action="add.php" method="post" autocomplete="off" enctype = "multipart/form-data">
   <div class="form__row">
     <label class="form__label" for="name">Название <sup>*</sup></label>
-
-    <input class="form__input" type="text" name="name" id="name" value="" placeholder="Введите название">
+    
+    <!-- input NAME -->
+    <input class="form__input" type="text" name="name" id="name" value="" placeholder="Введите название" required>
   </div>
 
   <div class="form__row">
     <label class="form__label" for="project">Проект <sup>*</sup></label>
 
+    <!-- input SELECT project -->
     <select class="form__input form__input--select" name="project" id="project">
 
       <?php
@@ -73,6 +101,7 @@ if (isset($_POST['name']) && isset($_POST['project']) && isset($_POST['date'])){
   <div class="form__row">
     <label class="form__label" for="date">Дата выполнения</label>
 
+    <!-- input DATE -->
     <input class="form__input form__input--date" type="text" name="date" id="date" value="" placeholder="Введите дату в формате ГГГГ-ММ-ДД">
   </div>
 
@@ -80,12 +109,15 @@ if (isset($_POST['name']) && isset($_POST['project']) && isset($_POST['date'])){
     <label class="form__label" for="file">Файл</label>
 
     <div class="form__input-file">
+
+      <!-- input FILE -->
       <input class="visually-hidden" type="file" name="file" id="file" value="">
 
       <label class="button button--transparent" for="file">
         <span>Выберите файл</span>
       </label>
     </div>
+
   </div>
 
   <div class="form__row form__row--controls">

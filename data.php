23ef025701c7cb46e108d98bd672092df69b8b3d
kirill_aleksandrov <kirill.alex.of@gmail.com>
2020-10-23 12:55:37 +0300

    project_task5

---
+++ data.php
@@ -0,0 +1,40 @@
+<?php
+$tasks = array(
+    array(
+        'name' => 'Собеседование в  IT компании',
+        'deadline' => '01.02.2019',
+        'type' => 'Работа',
+        'complete' => false,
+    ),
+    array(
+        'name' => 'Выполнить тестовое задание',
+        'deadline' => '25.12.2019',
+        'type' => 'Работа',
+        'complete' => false,
+    ),
+    array(
+        'name' => 'Сделать задание первого раздела',
+        'deadline' => '21.12.2019',
+        'type' => 'Учеба',
+        'complete' => true,
+    ),
+    array(
+        'name' => 'Встреча с другом',
+        'deadline' => '22.12.2019',
+        'type' => 'Входящие',
+        'complete' => false,
+    ),
+    array(
+        'name' => 'Купить корм для кота',
+        'deadline' => '',
+        'type' => 'Домашние дела',
+        'complete' => false,
+    ),
+    array(
+        'name' => 'Заказать пиццу',
+        'deadline' => '',
+        'type' => 'Домашние дела',
+        'complete' => false,
+    ),
+    );
+    ?>

4d26671cd98efb7c7accbc4c94ccc527c57beb33
kirill_aleksandrov <kirill.alex.of@gmail.com>
2020-11-04 00:15:09 +0300

    update

--- schema.sql
+++ schema.sql
@@ -2,13 +2,13 @@ CREATE DATABASE mydeal;
 CREATE TABLE projects (
     id INT NOT NULL AUTO_INCREMENT,
     project_name VARCHAR(50),
-    autor VARCHAR(30),
+    autor VARCHAR(30) UNIQUE,
     PRIMARY KEY(id)
 );
 CREATE TABLE tasks (
     id INT NOT NULL AUTO_INCREMENT,
     project_name VARCHAR(50),
-    autor VARCHAR(30),
+    autor VARCHAR(30) UNIQUE,
     start_date DATE,
     status BOOLEAN,
     task_name VARCHAR(50),
@@ -19,8 +19,8 @@ CREATE TABLE tasks (
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
     registration_date DATE,
-    email VARCHAR(50),
-    user_name VARCHAR(50),
+    email VARCHAR(50) UNIQUE,
+    user_name VARCHAR(50) UNIQUE,
     pass TEXT,
     PRIMARY KEY(id)
 );
